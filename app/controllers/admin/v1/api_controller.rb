da5341514db0b0cfa4d775bda18afddc4935c5ca
Luis Costa <luis.feliphe.pb@gmail.com>
2021-06-22 15:25:20 -0300

    versão funcional da autenticação

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,2 +1,5 @@
-class Admin::V1::ApiController < ApplicationController
+module Admin::V1
+    class ApiController < ApplicationController
+        include Authenticable
+    end
 end
--- app/controllers/storefront/v1/api_controller.rb
+++ app/controllers/storefront/v1/api_controller.rb
@@ -1,2 +1,5 @@
-class Storefront::V1::ApiController < ApplicationController
+module Storefront::V1
+    class ApiController < ApplicationController
+        include Authenticatable
+    end
 end
