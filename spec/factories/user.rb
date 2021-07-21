4ddc05df023b6d76e1f6f284430c14f0054baafc
Luis Costa <luis.feliphe.pb@gmail.com>
2021-07-20 21:09:54 -0300

    adding tests to API

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,21 @@
+# FactoryBot.define do
+#     factory :user do
+#       sequence(:name) { |n|  "basic #{n}" }
+#       name { Faker::Name.name}
+#       email { Faker::Internet.email}
+#       password {"123456"}
+#       password_confirmation {"1234556"}
+#       profile { %i(admin client).sample }
+#     end
+#   end
 FactoryBot.define do
-    factory :user do
-    sequence(:name) { |n|  "basic #{n}" }
-      name { Faker::Name.name}
-      email { Faker::Internet.email}
-      password {"123456"}
-      password_confirmation {"1234556"}
-      profile { %i(admin client).sample }
-    end
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "123456" }
+    password_confirmation { "123456" }
+    profile { :admin }
   end
+end
   
   
