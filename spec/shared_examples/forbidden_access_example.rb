489cdf859a0285693fe4dc379d4143ba55709159
Luis Costa <luis.feliphe.pb@gmail.com>
2021-07-21 22:02:13 -0300

    adding files

---
+++ spec/shared_examples/forbidden_access_example.rb
@@ -0,0 +1,10 @@
+shared_examples "forbidden access" do 
+
+    it "return error message" do 
+        expect(body_json['errors']['message']).to eq "Forbidden access"
+    end
+    
+    it "return forbidden status" do
+        expect(response).to have_http_status(:forbidden)    
+    end
+end
---
+++ spec/shared_examples/unauthenticated_access_example.rb
@@ -0,0 +1,7 @@
+shared_examples "unauthenticated access" do 
+
+    
+    it "return unauthorized status" do
+        expect(response).to have_http_status(:unauthorized)    
+    end
+end
